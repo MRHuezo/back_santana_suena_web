53644c51a258edc8863fbf820f575c2214c9d450
AldoChagollan <aldochagollan@outlook.com>
2023-01-28 21:27:17 -0600

    sedes

--- src/controllers/Sedes.controller.js
+++ src/controllers/Sedes.controller.js
@@ -44,7 +44,7 @@ SedeCtrl.editSede = async(req, res) => {
 }
 SedeCtrl.querySedes = async(req, res) => {
     try {
-        const sedes = await modelSede.find();
+        const sedes = await modelSede.find().sort({main: -1});
         res.status(200).json({ sedes: sedes });
     } catch (error) {
         console.log(error)
