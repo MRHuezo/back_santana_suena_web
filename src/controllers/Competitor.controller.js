1496166642499083b7c0ad9a939df997f67fa361
MRHuezo <mrhjonic@gmail.com>
2023-02-01 17:00:33 -0600

    competitororor

--- src/controllers/Competitor.controller.js
+++ src/controllers/Competitor.controller.js
@@ -1,41 +1,50 @@
 const ParticipanteCtrl ={};
 const modelCompetitor = require("../models/Competitor");
 const sendEmail = require("../middleware/sendEmail");
+const {
+    awsUploadImage
+  } = require("../middleware/awsFile");
 
 ParticipanteCtrl.createCompetitor = async(req, res) => {
     try {
-        //const { data } = req.body;
+        console.log(req.body);
+        //const data = req.body;
+       
+        res.status(200).json({ resp: 'success', message: 'Tu registro se realizó correctamente, se te ha enviado un correo confirmando tu registro.' });
+        return;
         const data ={ 
             name: 'Fractal Estudio Mx',
             id_sede:'63d0c6050bb0de7af0a603f6',
-            address: {
-                street: 'Hechos',
-                number: '22',
-                postal_code: '48900'
-            },
+            address: 'Hechos 22 22',
             email:'fractalestudiomx@gmail.com',
             url_video:'https://www.youtube.com/watch?v=icsr89F04Mw',
-            pay_image: '74983993',
-            personal_identification: '9364633',
+            pay: '74983993',
+            personal_identify: '9364633',
             phone: '3151526613',
             name_song: 'Flor de luna',
-            experience: 'Naci en una ciudad que reconocía su llegada para aportar al mundo...',
-            place_from: 'Autlán de Navarro',
+            artistic_experience: 'Naci en una ciudad que reconocía su llegada para aportar al mundo...',
+            from: 'Autlán de Navarro',
             birthday: '15/01/1990',
-            genre: 'Masculino'
+            genre: 'Masculino',
+            status:'INSCRITO'
         };  
-        
+      
         if(data.name === '' || data.id_sede === '' ||  data.address.street === ''||  data.address.number === ''
-        ||  data.address.postal_code ==='' ||  data.email === '' ||  data.url_video === '' ||  data.pay_image === '' 
+        ||  data.address.postal_code ==='' ||  data.email === '' ||  data.url_video === '' ||  data.pay.path === '' 
         ||  data.personal_identification === '' ||  data.phone  === '' ||  data.name_song === '' ||  data.experience === ''
         ||  data.place_from  === '' ||  data.birthday === '' || data.genre === '')  
         {  
             res.status(500).json({resp: 'error',message:'Todos los campos son necesarios.' });
             return;
         }
-        
-        sendMailToCompetitorFirstStage(data.email);
+        //const result = await awsUploadImage(data.pay, "pagos");
+     
+        //res.status(200).json({ resp: 'success', message: 'Tu registro se realizó correctamente, se te ha enviado un correo confirmando tu registro.' });
+        //return;
+        //Realizar validación email  y curp ya fueron registradas 
+        sendMailToCompetitorFirstStage(data.email, data.name);
         const competitor = await new modelCompetitor(data);
+
         competitor.save();
         //const participantes = await modelSede.find();
         res.status(200).json({ resp: 'success', message: 'Tu registro se realizó correctamente, se te ha enviado un correo confirmando tu registro.' });
@@ -44,15 +53,15 @@ ParticipanteCtrl.createCompetitor = async(req, res) => {
         console.log(error);
     }
 }
-const sendMailToCompetitorFirstStage = async (email)  =>{
+const sendMailToCompetitorFirstStage = async (email, name)  =>{
     try {
         console.log(email);
         // <a href="${urlReset}">${urlReset}</a>
         const htmlContentUser = `
                 <div>                    
                     <h3 style="font-family: sans-serif; margin: 15px 15px;">SANTANA SUENA</h3>
-                    <h4 style="font-family: sans-serif; margin: 15px 15px;">Hemos recibido tu inscripción pronto estaremos revisandolo. Pronto te comunicamos si eres seleccionado para la gran final. Mucha suerte.:</h4>
-                    
+                    <h4 style="font-family: sans-serif; margin: 15px 15px;">${name} Hemos recibido tu inscripción pronto estaremos revisandolo. Pronto te comunicamos si eres seleccionado para la gran final. Mucha suerte.:</h4>
+                 
 					             
                     <div style=" max-width: 550px; height: 100px;">
                         <p style="padding: 10px 0px;">SANTANA SUENA.</p>
--- src/middleware/awsFile.js
+++ src/middleware/awsFile.js
@@ -1,78 +1,136 @@
-const subir = {};
-const multer = require('multer');
-const aws = require('aws-sdk')
-const multerS3 = require('multer-s3')
+require("dotenv").config({path: ".env"});
+/* require('dotenv').config({silent: true}) */
+const AWS = require("aws-sdk");
 
-aws.config.update({
-    secretAccessKey: process.env.AWS_SECRET_ACCESS,
-    accessKeyId: process.env.AWS_ACCESS_ID,
+//const ID = process.env.AWS_ACCESS_ID;
+//const SECRET = process.env.AWS_SECRET_ACCESS;
+//const BUCKET = process.env.AWS_BUCKET_NAME;
+
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION
-})
+});
 
-const s3 = new aws.S3();
+const s3 = new AWS.S3();
 
-//Filtros que se aceptaran en los archivos
-const fileFilter = (req, file, cb) => {
-    console.log(file);
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/webp' ) {
-        cb(null, true);
-    } else {
-        return cb(new Error('Formato no valido'));
+/* const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS,
+
+}) */
+
+async function awsUploadImage(file, folder) {
+    try {
+        let params = { 
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: "",
+            Body: file
+        }
+
+        const { createReadStream, filename, mimetype } = await file;
+        // Invoking the `createReadStream` will return a Readable Stream.
+        // See https://nodejs.org/api/stream.html#stream_readable_streams
+        const stream = createReadStream();
+
+        stream.on("error", (error) => console.error(error));
+        params.Body = stream;
+        let timestamp = new Date().getTime();
+        const round_num = Math.floor((Math.random() * 5) + 1);
+        // get the file extension.
+        const file_extension = mimetype.split('/')[1];
+        const final_key = `${timestamp}${round_num}`;
+        params.Key = `${folder}/${final_key}.${file_extension}`;
+        // upload the object.
+        const result = await s3.upload(params).promise();
+
+        /* const params = { 
+            Bucket: BUCKET,
+            Key: `${filepath}`,
+            Body: file
+        } */
+
+        //const response = await s3.upload(params).promise();
+        return result.Location;
+    } catch (error) {
+        console.log(error);
+        throw new Error();
     }
-  }
-  
+}
+async function awsUploadImageMultiple(files,folder){
+    // create an object containing the name of the bucket, the key, body, and acl of the object.
+    let params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: "",
+        Body: files
+    };
 
-  //Aqui es donde conectamos al Bucket de Amazon S3 y le damos los filtros
-  const configuracionMulter = {
-    fileFilter,
-    storage: multerS3({
-      s3: s3,
-      bucket: process.env.NAME_BUCKET_AMS,
-      acl: 'public-read',
-      metadata: function (req, file, cb) {
-        cb(null, {fieldName: 'Testing_metadata'});
-      },
-      key: function (req, file, cb) {
-        cb(null, Date.now().toString())
-      }
-    }) 
-  };
+    let folder_location = [
+        "productos",
+        "empresas",
+        "usuarios"
+    ];
 
-  const configuracionMulterInFilter = {
-    storage: multerS3({
-      s3: s3,
-      bucket: process.env.NAME_BUCKET_AMS,
-      acl: 'public-read',
-      metadata: function (req, file, cb) {
-        console.log("metadata",file);
-        cb(null, {fieldName: 'Testing_metadata'});
-      },
-      key: function (req, file, cb) {
-        console.log("key",file);
-        const type = file.originalname.split(".");
-        cb(null, Date.now().toString() + `.${type[1]}`)
-      }
-    }) 
-  };
+    // structure the return data.
+    let objects = [];
 
+    // loop through all the sent files.
+    for(let i = 0; i < files.length; i++){
+        let file = files[i];
+        // From the file, get the read stream and the filename.
+        let {createReadStream, filename , mimetype} = await file;
+        // read the data from the file.
+        let stream = createReadStream();
+        // in case of any error, log it.
+        stream.on("error", (error) => console.error(error));
+        // assign the body of the object to the data to read.
+        params.Body = stream;
+        // get the current timestamp.
+        let timestamp = new Date().getTime();
+        const round_num = Math.floor((Math.random() * 5) + 1);
+        // get the file extension.
+        const file_extension = mimetype.split('/')[1];
+        const final_key = `${timestamp}${round_num}`;
+        // compose the key as the folder name, the timestamp, and the file extension of the object.
+        params.Key = `${folder_location[folder]}/${final_key}.${file_extension}`;
+        // upload the object.
+        const result = await s3.upload(params).promise();
+        // push the structured response to the objects array.
+        objects.push({
+            url_imagen: result.Location,
+            location_imagen: params.Key,
+            key_imagen: final_key,
+            extencion_imagen: file_extension
+        });
+    };
+    // return the response to the client.
+    return objects;
 
-//Funcion que elimina la imagen deL Bucket
-subir.eliminarImagen = (keyDeleted) => {
-    s3.deleteObject({
-      Bucket: process.env.NAME_BUCKET_AMS,
-      Key: keyDeleted
-    },function(err, data) {
-      if(err){
-        throw err;
-      } 
-    })
-  }
+};
+ 
+const eliminarImagenesAWS = async (files) => {
+    for(var i=0; i < files.length; i++){
+        const img = files[i];
+        s3.deleteObject({
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: img.key_imagen,
+        },function(err, data) {
+            if(err){
+                throw err;
+            } 
+        })
+    }
+}
 
-subir.upload = multer(configuracionMulter).single('imagen');
+ const eliminarUnaImagen = async (keyDeleted) => {
+     s3.deleteObject({
+       Bucket: process.env.AWS_BUCKET_NAME,
+       Key: keyDeleted
+     },function(err, data) {
+       if(err){
+         throw err;
+       } 
+     })
+   }
 
-subir.uploadMultiple = multer(configuracionMulter).array('images', 2);
-
-subir.uploadFile = multer(configuracionMulterInFilter).single('file');
-
-
-module.exports = subir;
+module.exports = { awsUploadImage, awsUploadImageMultiple, eliminarImagenesAWS, eliminarUnaImagen };
