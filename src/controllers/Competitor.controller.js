6ba96f967e50fafedb1c4070ffdede3a0f52a8d4
MRHuezo <mrhjonic@gmail.com>
2023-02-03 14:37:45 -0600

    cambiosAddCompetitor

--- src/controllers/Competitor.controller.js
+++ src/controllers/Competitor.controller.js
@@ -1,105 +1,78 @@
-const ParticipanteCtrl = {};
+const ParticipanteCtrl ={};
 const modelCompetitor = require("../models/Competitor");
 const sendEmail = require("../middleware/sendEmail");
 const uploadFileAws = require("../middleware/awsFile");
 
 ParticipanteCtrl.uploadFileMultiple = async (req, res, next) => {
-  try {
-    await uploadFileAws.uploadInscription(req, res, function (err) {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({ message: err.message });
-      }
-      return next();
-    });
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-};
-
-ParticipanteCtrl.createCompetitor = async (req, res) => {
-  try {
-    const data = req.body;
-    console.log(req.files);
-    console.log({ req: req.body });
-    /* 
-        const data ={ 
-            name: 'Fractal Estudio Mx',
-            id_sede:'63d0c6050bb0de7af0a603f6',
-            address: 'Hechos 22 22',
-            email:'fractalestudiomx@gmail.com',
-            url_video:'https://www.youtube.com/watch?v=icsr89F04Mw',
-            pay: '74983993',
-            personal_identify: '9364633',
-            phone: '3151526613',
-            name_song: 'Flor de luna',
-            artistic_experience: 'Naci en una ciudad que reconocía su llegada para aportar al mundo...',
-            from: 'Autlán de Navarro',
-            birthday: '15/01/1990',
-            genre: 'Masculino',
-            status:'INSCRITO'
-        };   */
-
-    if (
-      data.name === "" ||
-      data.id_sede === "" ||
-      data.address.street === "" ||
-      data.address.number === "" ||
-      data.address.postal_code === "" ||
-      data.email === "" ||
-      data.url_video === "" ||
-      //data.pay.path === "" ||
-      //data.personal_identification === "" || 
-      //cambia estas validaciones por las Arrays de imgs, 
-      //en el body ya no vienen
-      data.phone === "" ||
-      data.name_song === "" ||
-      data.experience === "" ||
-      data.place_from === "" ||
-      data.birthday === "" ||
-      data.genre === ""
-    ) {
-      throw new Error("Todos los campos son necesarios.");
-    }
-
-    /* console.log(await upload(req, res, function (err) {
+    try {
+      await uploadFileAws.uploadInscription(req, res, function (err) {
         if (err) {
-          res.status(500).json({ message: err });
+          console.log(err);
+          return res.status(500).json({ message: err.message });
         }
-    
-        return res.json({
-            success: true,
-            message: 'Image uploaded!'
-        });
+        return next();
+      });
+    } catch (error) {
+        console.log(error)
+      res.status(500).json({ message: error });
+    }
+  };
+  
+ParticipanteCtrl.createCompetitor = async(req, res) => {
+    try {
+        const {body, files} = req;
+        const {pay,personal_identify, photo} = files;
 
-      }));  */
-    //res.status(500).json({resp: 'error',message:'Todos los campos son necesarios.' });
-    return;
-    //const result = await awsUploadImage(data.pay, "pagos");
+        const data ={ 
+            ...body,
+            pay_key: pay[0].key,
+            pay: pay[0].location,
+            personal_identify_key: personal_identify[0].key,
+            personal_identify: personal_identify[0].location,
+            photo_key:photo[0].key,
+            photo:photo[0].location,
+            status:'INSCRITO'
+        };   
+      
+        if(data.name === '' || data.id_sede === '' ||  data.address === ''||  data.email === '' || 
+            data.url_video === '' ||  data.pay === '' ||  data.personal_identification === ''  || 
+            data.phone  === '' ||  data.name_song === '' ||  data.experience === '' ||data.photo === '' ||
+            data.place_from  === '' ||  data.birthday === '' || data.genre === '' || data.curp === '')  
+        {  
+            res.status(500).json({resp: 'error',message:'Todos los campos son necesarios.' });
+            return;
+        }
+  
+       const existCurp = await modelCompetitor.find({curp: data.curp});
+       const existEmail = await modelCompetitor.find({email: data.email});
+       
+       console.log({existCurp, existEmail});
+        if(existCurp.length > 0 ) {
+            res.status(400).json({resp: 'error',message:'Ya se registró un participante con esta CURP.' });
+            return;
+        }
+        if( existEmail.length > 0) {
+            res.status(400).json({resp: 'error',message:'Ya se registró un participante con este email.' });
+            return;
+        }
 
-    //res.status(200).json({ resp: 'success', message: 'Tu registro se realizó correctamente, se te ha enviado un correo confirmando tu registro.' });
-    //return;
-    //Realizar validación email  y curp ya fueron registradas
-    sendMailToCompetitorFirstStage(data.email, data.name);
-    const competitor = await new modelCompetitor(data);
+        const competitor = await new modelCompetitor(data);
 
-    competitor.save();
-    //const participantes = await modelSede.find();
-    res.status(200).json({
-      resp: "success",
-      message:
-        "Tu registro se realizó correctamente, se te ha enviado un correo confirmando tu registro.",
-    });
-  } catch (error) {
-    res.status(500).json({ message: error });
-    console.log(error);
-  }
-};
-const sendMailToCompetitorFirstStage = async (email, name) => {
-  try {
-    console.log(email);
-    // <a href="${urlReset}">${urlReset}</a>
-    const htmlContentUser = `
+        competitor.save();
+        sendMailToCompetitorFirstStage(data.email, data.name);
+        //const participantes = await modelSede.find();
+        res.status(200).json({ resp: 'success', message: 'Tu registro se realizó correctamente, se te ha enviado un correo confirmando tu registro.' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error });
+      
+    }
+}
+const sendMailToCompetitorFirstStage = async (email, name)  =>{
+    try {
+        console.log(email);
+        // <a href="${urlReset}">${urlReset}</a>
+        const htmlContentUser = `
                 <div>                    
                     <h3 style="font-family: sans-serif; margin: 15px 15px;">SANTANA SUENA</h3>
                     <h4 style="font-family: sans-serif; margin: 15px 15px;">${name} Hemos recibido tu inscripción pronto estaremos revisandolo. Pronto te comunicamos si eres seleccionado para la gran final. Mucha suerte.:</h4>
@@ -116,25 +89,79 @@ const sendMailToCompetitorFirstStage = async (email, name) => {
       htmlContentUser,
       "Santana Suena"
     );
-  } catch (error) {
-    console.log(error);
-  }
-};
-ParticipanteCtrl.queryParticipantes = async (req, res) => {
-  try {
-    const competitor = await modelCompetitor.find().populate("id_sede");
-    res.status(200).json({ competitor });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: error.message });
-  }
-};
-ParticipanteCtrl.seleccionarParticipante = async (req, res) => {
-  try {
-    const sedes = await modelSede.find();
-    res.status(200).json({ sedes: sedes });
-  } catch (error) {
-    console.log(error);
-  }
-};
+    } catch (error) {
+        console.log(error)
+    }
+}
+ParticipanteCtrl.queryParticipantes = async(req, res) => {
+    try {
+      console.log(req.params);
+      let OR = [];
+      let AND = [];
+      if(req.params.competitor){
+        OR.push({ name: { $regex: ".*" + req.params.competitor + ".*", $options: "i" } })
+      }
+     
+      console.log(OR)
+      let aggregateFind;
+
+      if(req.params.competitor){
+        aggregateFind = await modelCompetitor.aggregate([
+            {
+                $match: {
+                    $or: OR,
+                  
+                },
+            },
+        ]);
+      }else{
+        if(req.params.id_sede){
+            aggregateFind = await modelCompetitor.find({ id_sede:req.params.id_sede }); 
+        }else{
+            aggregateFind = await modelCompetitor.find();
+        }
+       
+      }
+         
+        const competitor = await modelCompetitor.populate(aggregateFind, {
+            path: "id_sede",
+        });
+       
+        res.status(200).json({ competitor });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: error.message });
+    }
+}
+
+ParticipanteCtrl.accept = async(req, res) => {
+    try {
+        const sedes = await modelSede.find();
+        res.status(200).json({ sedes: sedes });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+ParticipanteCtrl.decline = async(req, res) => {
+    try {
+        const sedes = await modelSede.find();
+        res.status(200).json({ sedes: sedes });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+ParticipanteCtrl.seleccionarParticipante = async(req, res) => {
+    try {
+        const sedes = await modelSede.find();
+        res.status(200).json({ sedes: sedes });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
 module.exports = ParticipanteCtrl;
+
--- src/routes/Competitor.js
+++ src/routes/Competitor.js
@@ -4,11 +4,19 @@ const router = Router();
 const { 
     uploadFileMultiple,
     createCompetitor,
-    queryParticipantes
+    queryParticipantes,
+    accept,
+    decline
  } = require('../controllers/Competitor.controller.js');
 
 router.route("/create").post(uploadFileMultiple, createCompetitor);
-router.route("/get").get(queryParticipantes);
+router.route("/get/main/").get(queryParticipantes);
+router.route("/get/main/:competitor").get(queryParticipantes);
+router.route("/get/sede/:id_sede").get(queryParticipantes);
+router.route("/get/:competitor/:id_sede").get(queryParticipantes);
+
+router.route('/edit/accept/:id_competitor').put(accept);
+router.route('/edit/decline/:id_competitor').put(decline);
 
 
 module.exports = router;
